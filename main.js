c0cf5de67511cb9e22b1a9a1228fb138618128d3
Matheus Fonseca <matheusfonsecamorais@gmail.com>
2019-12-22 23:13:14 -0300

    Adicionando Clientes

--- main.js
+++ main.js
@@ -1,51 +1,82 @@
+class LocalStorageCrtl {
+
+  constructor () {
+    this.clientes = this.getClientes();
+  }
+
+  getClientes() {
+    if(localStorage.getItem('clientes') === null) {
+      return [];
+    } else {
+      return JSON.parse(localStorage.getItem('clientes'));
+    }
+  }
+
+  saveCliente(cliente) {
+    this.clientes = this.getClientes();
+    this.clientes.push(cliente);
+    localStorage.setItem('clientes', JSON.stringify(this.clientes));
+  }
+
+}
+
 class UI {
 
   constructor () {
-    this.clientes = [];
     this.uiSelectors = {
       addClientBtn: '#addClientBtn',
       addClientInput: '#addClientInput'
     };
-    this.getClientes();
-  }
-
-  getClientes(){
-    if(localStorage.getItem('clientes') === null) {
-      this.clientes = [];
-    } else {
-      this.clientes = JSON.parse(localStorage.getItem('clientes'));
-    }
   }
 
   getSelectors () {
     return this.uiSelectors;
   }
 
+  clearInput (selector) {
+    document.querySelector(this.uiSelectors[selector]).value = '';
+  }
 }
 
 class ClienteCtrl{
 
-  constructor (UI) {
+  constructor (UI, localStorageCrtl) {
     this.ui = UI;
+    this.lsCrtl = localStorageCrtl;
     this.uiSelectors = this.ui.getSelectors();
+    this.clientes = localStorageCrtl.getClientes();
+    this.idGenerator = () => Math.random().toString(36).substr(2, 9);
   }
 
   addCliente(e) {
 
+    e.preventDefault();
     const addClientInput = document.querySelector(this.uiSelectors.addClientInput);
     const nomeCliente = addClientInput.value;
 
-    // Adicionar na localStorage - Criar um lsCtrl
+    // filtrando somente o nome dos clientes
+    this.cliente = this.lsCrtl.getClientes();
+    const nomes = this.clientes.map(cliente => cliente.nome.toLowerCase());
+    
+    // Se o nome nao for repetido E nao for vazio
+    if(!nomes.includes(nomeCliente.toLowerCase()) && nomeCliente != ''){
+
+      const cliente = {
+        id: this.idGenerator(),
+        nome: nomeCliente
+      }
+      this.clientes.push(cliente);
+      this.lsCrtl.saveCliente(cliente);
+    }
+
+    this.ui.clearInput('addClientInput');
 
     e.preventDefault();
-    console.log(nomeCliente);
  
   }
 
 }
 
-// const arrClientes = ["Ana", "Maria"];
-// localStorage.setItem('clientes', JSON.stringify(arrClientes));
 
 // Inicializando...
 document.addEventListener('DOMContentLoaded', app());
@@ -56,7 +87,8 @@ function app() {
   // Inicializando controllers
   const ui = new UI();
   const uiSelectors = ui.getSelectors();
-  const clienteCtrl = new ClienteCtrl(ui);
+  const localStorageCrtl = new LocalStorageCrtl();
+  const clienteCtrl = new ClienteCtrl(ui, localStorageCrtl);
 
   // Adicionando listeners
   document.querySelector(uiSelectors.addClientBtn).addEventListener('click', evt => clienteCtrl.addCliente(evt)); // Estudar por que a arrow function muda o 'this' dentro do event handler
